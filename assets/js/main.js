a920c698b7ee1c6cc92c311bd4602a1eec7d621e
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-06 17:33:33 +0000

    se agregan componentes js

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+$('#btn1').click(function() {
+    alert("EL MENSAJE FUE ENVIADO CORRECTAMENTE...")
+ })
+
+ 
